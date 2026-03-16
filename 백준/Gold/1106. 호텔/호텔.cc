d63a33470cd0f41ba008413b52fdc58932bfbc53
Jaehyun Byun <143696056+DevByn@users.noreply.github.com>
2026-03-17 06:44:01 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -1,31 +1,64 @@
-#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <string>
+#include <climits>
+#include <cstdio>
+#include <stack>
+#include <cstring>
+
 using namespace std;
+const int INF{ INT_MAX };
+
+struct City {
+    int cost;
+    int customer;
+};
+
+int C, N;
+City CITIES[20];
 
 int main() {
-    int C, N;
-    cin >> C >> N;
+    ios::sync_with_stdio(false); //앞은 현재 도시, 뒤는 현재 선택한 개수
+    cin.tie(nullptr);
 
-    vector<pair<int,int>> city(N);
-    for (auto& [c, g] : city) cin >> c >> g;
-
-    int maxGain = 0;
-    for (auto [c, g] : city) maxGain = max(maxGain, g);
-
-    const int INF = 1e9;
-    vector<int> dp(C + maxGain, INF);
-    dp[0] = 0;
-
-    for (int j = 1; j < C + maxGain; j++) {
-        for (auto [cost, gain] : city) {
-            int prev = max(0, j - gain);
-            if (dp[prev] != INF)
-                dp[j] = min(dp[j], dp[prev] + cost);
+    //freopen("input.txt", "r", stdin);
+  
+    cin >> C >> N; 
+    int max_gain{ 0 };
+    for (int i = 0; i < N ; i++) {
+        cin >> CITIES[i].cost >> CITIES[i].customer;
+        if (CITIES[i].customer > max_gain) {
+            max_gain = CITIES[i].customer;
         }
     }
 
-    int ans = INF;
-    for (int j = C; j < C + maxGain; j++)
-        ans = min(ans, dp[j]);
 
-    cout << ans;
+    vector<int>DP(C + max_gain,INF);
+
+    for (auto& j : CITIES) {
+        DP[j.customer] = min(DP[j.customer],j.cost);
+    }
+
+    for (int i = 0; i < C + max_gain; i++) {
+        for (auto &j: CITIES) {
+            if (i - j.customer >= 0 && DP[i - j.customer] != INF ) {
+                DP[i] = min(DP[i], DP[i - j.customer] + j.cost);
+            }
+        }
+    }
+    
+    int min_ans{ INF };
+
+
+    for (int i = C; i < C + max_gain; i++) {
+        if (DP[i] < min_ans) {
+            min_ans = DP[i];
+        }
+    }
+    cout << min_ans;
 }
