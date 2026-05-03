972516261366be8f1e7d512b4de69ffb60077281
Jaehyun Byun <143696056+DevByn@users.noreply.github.com>
2026-05-03 21:54:39 +0900

    [level 2] Title: 모음 사전, Time: 0.11 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -2,39 +2,8 @@
 #include <vector>
 
 using namespace std;
-vector<char>CHAR = {'A', 'E', 'I', 'O', 'U'};
-int cnt {0};
-
-int ans {0};
-
-void backtracking(int length, string now, const string& word){
-    
-    if(now == word){
-        ans = cnt;
-        return;
-    }
-    
-    if(length == 5){
-        return;
-    }
-    
-    for(int i = 0; i < 5; i++){
-        
-        now.push_back(CHAR[i]);
-        cnt++;
-        backtracking(length + 1, now, word);
-        now.pop_back();
-        
-    }
-    
-}
 
 int solution(string word) {
     int answer = 0;
-    
-    backtracking(0, "", word);
-    
-    answer = ans;
-    
     return answer;
 }
