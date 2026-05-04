48b39067e40ad4178a86c3b113db371967639566
Jaehyun Byun <143696056+DevByn@users.noreply.github.com>
2026-05-04 21:39:03 +0900

    [level 2] Title: 구명보트, Time: 1.78 ms, Memory: 4.77 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -1,28 +1,9 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> people, int limit) {
     int answer = 0;
-    answer = people.size();
-    sort(people.begin(),people.end());
-    
-    int left = 0; int right = people.size() - 1; int coupled{0};
-    
-    while(left < right){
-        if(people[left] + people[right] <= limit){
-            left++; right--;
-            coupled++;
-        }
-        else if(people[left] + people[right] > limit){
-            right--;
-        }
-    }
-    
-    answer -= coupled;
-    
     return answer;
 }
